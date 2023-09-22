3d4e569d54a8af4ea2fa62d7cc4670badab2c2e7
manii9999 <ymanikanta6264@gmail.com>
2023-09-22 15:42:29 +0530

    first

---
+++ logsbkp.py
@@ -0,0 +1,15 @@
+import os
+from datetime import date
+
+
+os.popen("sudo su -")
+
+now=date.today()
+
+
+
+print("Back-up the logs under the root Directory")
+directory="Logs_bkp_{}".format(now)
+os.popen("sudo mkdir /root/%s"%(directory))
+os.popen("sudo cp /var/log/auth* /root/%s"%(directory))
+print("Failed logs backup")
