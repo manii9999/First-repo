12077e2793ac67d8dadfe2d141665048aa611948
manii9999 <ymanikanta6264@gmail.com>
2023-09-23 07:16:59 +0530

    no change

--- logsbkp.py
+++ logsbkp.py
@@ -13,3 +13,6 @@ directory="Logs_bkp_{}".format(now)
 os.popen("sudo mkdir /root/%s"%(directory))
 os.popen("sudo cp /var/log/auth* /root/%s"%(directory))
 print("Failed logs backup")
+
+print("No Changes")
+
